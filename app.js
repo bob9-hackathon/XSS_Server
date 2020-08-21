9653fd87745a4399178cb628ceec4d9e005ac1fa
nnnlog <sorisem4106@naver.com>
2020-08-22 03:22:57 +0900

    upload server src

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let html = param => `<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+<title>XSS TEST</title>
+</head>
+<body>
+${param}
+</body>
+`;
+
+app.use((req, res, next) => {
+	res.header('Content-type', 'text/html');
+	next();
+});
+
+app.get("/xss_get", (req, res) => {
+	console.log(req.query)
+	res.end(html(req.query.xss));
+});
+
+app.post("/xss_post", (req, res) => {
+	res.end(html(req.body.xss));
+});
+
+app.listen(80, () => console.log('XSS Victim Server is running on port 80.'));
